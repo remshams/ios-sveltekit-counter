256206e92704b9e6cd83af54c50bbf5a1d731c6d
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-05-13 17:56:17 +0200

    * extracts dedicated counter view

---
+++ Shared/Counter.swift
@@ -0,0 +1,86 @@
+//
+//  Counter.swift
+//  sveltekit-counter
+//
+//  Created by Mathias Remshardt on 13.05.21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Counter: View {
+  @State var count = 0
+  @State var offset = 0
+  @State var nextCount = 0
+  private let fontsize = 120
+  
+  var body: some View {
+    HStack {
+      CounterButton(count: $nextCount, offset: $offset, sfImage: Image(systemName: "minus"), offsetChange: -fontsize, countChange: -1)
+      CounterDisplay(count: count, offset: offset, fontsize: fontsize)
+      CounterButton(count: $nextCount, offset: $offset, sfImage: Image(systemName: "plus"), offsetChange: fontsize, countChange: 1)
+    }.onAnimationCompleted(for: CGFloat(offset)) {
+      count = nextCount
+      offset = 0
+    }
+  }
+}
+
+private struct CounterButton: View {
+  @Binding var count: Int
+  @Binding var offset: Int
+  let sfImage: Image
+  let offsetChange: Int
+  let countChange: Int
+  
+  var body: some View {
+    Button(action: {
+      withAnimation(.spring(response: 0.5, dampingFraction: 0.4)) {
+        count += countChange
+        offset += offsetChange
+      }
+    }) {
+      sfImage.font(.system(size: 40))
+    }
+  }
+}
+
+private struct CounterDisplay: View {
+  let count: Int
+  let offset: Int
+  let fontsize: Int
+  
+  var body: some View {
+    ZStack {
+      ForEach(1..<20) { index in
+        CounterText(count: count + index, offset: -fontsize * index, fontsize: fontsize)
+      }
+      CounterText(count: count, offset: 0, fontsize: fontsize)
+      ForEach(1..<20) { index in
+        CounterText(count: count - index, offset: fontsize * index, fontsize: fontsize)
+      }
+    }
+    .offset(y: CGFloat(offset))
+  }
+}
+
+private struct CounterText: View {
+  let count: Int
+  let offset: Int
+  let fontsize: Int
+  
+  var body: some View {
+    Text("\(count)")
+      .font(.system(size: CGFloat(fontsize)))
+      .foregroundColor(.red)
+      .offset(y: CGFloat(offset))
+  }
+}
+
+#if DEBUG
+struct Counter_Previews: PreviewProvider {
+  static var previews: some View {
+    Counter()
+  }
+}
+#endif
--- Shared/SvelteKitCounter.swift
+++ Shared/SvelteKitCounter.swift
@@ -9,67 +9,34 @@ import SwiftUI
 
 struct SvelteKitCounter: View {
   
-  @State var nextCount = 0
-  @State var count = 0
-  @State var offset = 0
   
   var body: some View {
-    HStack {
-      Button(action: {
-        withAnimation(.spring(response: 0.5, dampingFraction: 0.4)) {
-          nextCount -= 1
-          offset -= 120
-        }
-      }) {
-        Image(systemName: "minus").font(.system(size: 40))
-      }
-      VStack(spacing: 0) {
-        Rectangle()
-          .fill(Color.white)
-          .frame(width: 120)
-          .ignoresSafeArea()
-          .zIndex(1)
-        ZStack {
-          ForEach(1..<12) { index in
-            Count(count: count + index, yOffset: -120 * index)
-          }
-          Count(count: count, yOffset: 0)
-          ForEach(1..<12) { index in
-            Count(count: count - index, yOffset: 120 * index)
-          }
-        }.offset(y: CGFloat(offset))
-        Rectangle()
-          .fill(Color.white)
-          .frame(width: 120)
-          .ignoresSafeArea()
-          .zIndex(1)
-      }
-      
-      Button(action: {
-        withAnimation(.spring(response: 0.5, dampingFraction: 0.4)) {
-          nextCount += 1
-          offset += 120
-        }
-        
-      }) {
-        Image(systemName: "plus").font(.system(size: 40))
-      }
-    }.onAnimationCompleted(for: CGFloat(offset)) {
-      count = nextCount
-      offset = 0
+    VStack {
+      Backdrop().zIndex(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+      Counter()
+      Backdrop()
     }
   }
 }
 
-struct Count: View {
-  let count: Int
-  let yOffset: Int
+struct Backdrop: View {
+  let content: AnyView
+  
+  init<Content: View>(@ViewBuilder content: () -> Content) {
+    self.content = AnyView(content())
+  }
+  
+  init() {
+    self.init {
+      EmptyView()
+    }
+  }
   
   var body: some View {
-    Text("\(count)")
-      .font(.system(size: 120))
-      .foregroundColor(.red)
-      .offset(y: CGFloat(yOffset))
+    ZStack {
+      Rectangle().fill(Color.white).ignoresSafeArea()
+      content
+    }
   }
 }
 
