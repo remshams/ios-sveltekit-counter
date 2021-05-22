a96a316d78743bbd1f13c80b7b6376f79e80fd7e
Mathias Remshardt <mathias.remshardt@logicline.de>
2021-05-22 18:27:24 +0200

    * refactoring animation handling to CounterDisplay in CounterView

--- Shared/Counter.swift
+++ Shared/Counter.swift
@@ -9,68 +9,48 @@ import Foundation
 import SwiftUI
 
 struct Counter: View {
-  @State var count = 0
   @State var offset = 0
-  @State var offsetOnAnimationStart = 0
   @State var countChange = 0
-
-  private let fontsize = 120
+  let fontsize = 120
+  let offsetStep = 120
 
   var body: some View {
     HStack {
       Spacer()
       CounterButton(
-        count: $countChange,
+        countChange: $countChange,
         offset: $offset,
         sfImage: Image(systemName: "minus"),
-        offsetChange: -fontsize,
-        countChange: -1
+        offsetStep: -offsetStep,
+        countStep: -1
       )
       Spacer()
-      CounterDisplay(count: count, offset: offset, fontsize: fontsize).gesture(DragGesture()
-        .onChanged {
-          offset = offsetOnAnimationStart + Int($0.translation.height)
-        }.onEnded { _ in
-          withAnimation(.spring(response: 0.5, dampingFraction: 0.4)) {
-            let remainder = abs(offset % fontsize)
-            let diff = remainder > fontsize / 2 ? fontsize - remainder : remainder * -1
-            offset = offset > 0 ? offset + diff : offset - diff
-            offsetOnAnimationStart = offset
-
-            countChange = offset / fontsize
-          }
-
-        })
+      CounterDisplay(fontsize: fontsize, offsetStep: offsetStep, offset: $offset, countChange: $countChange)
       Spacer()
       CounterButton(
-        count: $countChange,
+        countChange: $countChange,
         offset: $offset,
         sfImage: Image(systemName: "plus"),
-        offsetChange: fontsize,
-        countChange: 1
+        offsetStep: offsetStep,
+        countStep: 1
       )
       Spacer()
-    }.onAnimationCompleted(for: CGFloat(offset)) {
-      count += countChange
-      countChange = 0
-      offset = 0
-      offsetOnAnimationStart = 0
     }
   }
 }
 
 private struct CounterButton: View {
-  @Binding var count: Int
+  @Binding var countChange: Int
   @Binding var offset: Int
   let sfImage: Image
-  let offsetChange: Int
-  let countChange: Int
+  let offsetStep: Int
+  let countStep: Int
 
   var body: some View {
     Button(action: {
       withAnimation(.spring(response: 0.5, dampingFraction: 0.4)) {
-        count += countChange
-        offset += offsetChange
+        countChange += countStep
+        offset += offsetStep
       }
     }, label: {
       sfImage.font(.system(size: 40)).foregroundColor(.black)
@@ -79,22 +59,60 @@ private struct CounterButton: View {
 }
 
 private struct CounterDisplay: View {
-  let count: Int
-  let offset: Int
   let fontsize: Int
+  let offsetStep: Int
+
+  @Binding var offset: Int
+  @Binding var countChange: Int
+
+  @State var count: Int = 0
+  @State var offsetOnAnimationStart = 0
 
   var body: some View {
     ZStack {
       ForEach(1 ..< 20) { index in
-        CounterText(count: count + index, offset: -fontsize * index, fontsize: fontsize)
+        CounterText(count: count + index, offset: -offsetStep * index, fontsize: fontsize)
       }
       CounterText(count: count, offset: 0, fontsize: fontsize)
       ForEach(1 ..< 20) { index in
-        CounterText(count: count - index, offset: fontsize * index, fontsize: fontsize)
+        CounterText(count: count - index, offset: offsetStep * index, fontsize: fontsize)
       }
     }
     .frame(minWidth: CGFloat(fontsize + 80))
     .offset(y: CGFloat(offset))
+    .gesture(DragGesture()
+      .onChanged {
+        offset = offsetOnAnimationStart + Int($0.translation.height)
+      }.onEnded { _ in
+        withAnimation(.spring(response: 0.5, dampingFraction: 0.4)) {
+          // "abs" as difference can be positive or negative depending on the drag direction
+          let distanceFromLastFullStep = abs(offset % offsetStep)
+          offset = self.nomalizeToOffsetStep(distanceFromLastFullStep: distanceFromLastFullStep)
+          
+          // The count is equivalent to the number of complete steps
+          countChange = offset / offsetStep
+        }
+
+      })
+    /**
+     * Is re required as count can also be increased by buttons (not just the drag gesture)
+     * With the help of "onAnimationCompleted" the same logic both for the count change by button or drag animation can be used
+     */
+    .onAnimationCompleted(for: CGFloat(offset)) {
+      // Increase/Descrease count by the number of complete steps
+      count += countChange
+      
+      // Reset animation state variables
+      countChange = 0
+      offset = 0
+      offsetOnAnimationStart = 0
+    }
+  }
+
+  private func nomalizeToOffsetStep(distanceFromLastFullStep distance: Int) -> Int {
+    // Go up if closes to next full step, otherwise go down
+    let difference = distance > offsetStep / 2 ? offsetStep - distance : distance * -1
+    return offset > 0 ? offset + difference : offset - difference
   }
 }
 
